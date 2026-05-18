d924ba59779568062c87f240dd1b097d2b46accf
Matrix9180 <matrix9180@proton.me>
2026-05-18 09:29:26 -0700

    Bundler shim: expand rubygems integration

--- bundler/rubygems_integration.rb
+++ bundler/rubygems_integration.rb
@@ -14,10 +14,28 @@ module Bundler
       Gem.path
     end
 
+    def platforms
+      Gem.platforms
+    end
+
     def loaded_specs(name = nil)
       specs = Gem.loaded_specs
       return specs if name.nil?
       specs[name]
+    end
+
+    def all_specs
+      specs = []
+      Gem::Specification.each { |spec| specs << spec }
+      specs
+    end
+
+    def installed_specs
+      all_specs
+    end
+
+    def find_name(name)
+      all_specs.select { |spec| spec.name == name.to_s }
     end
   end
 end
--- tests/cases/bundler_rubygems_integration.rb
+++ tests/cases/bundler_rubygems_integration.rb
@@ -7,4 +7,9 @@ puts integration.is_a?(Bundler::RubygemsIntegration)
 puts integration.gem_dir == Gem.dir
 puts integration.gem_bindir == Gem.bindir
 puts integration.default_path.is_a?(Array)
+puts integration.platforms[0] == "ruby"
+puts integration.loaded_specs.is_a?(Hash)
+puts integration.all_specs.is_a?(Array)
+puts integration.installed_specs.is_a?(Array)
+puts integration.find_name("definitely-missing").length
 puts Bundler.respond_to?(:rubygems)
