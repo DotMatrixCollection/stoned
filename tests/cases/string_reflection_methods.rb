a4b308c49f3e1f9baedb8c29c3580f39e49783fd
Matrix9180 <matrix9180@proton.me>
2026-05-31 20:01:38 -0700

    Advertise String mutator methods

--- tests/cases/string_reflection_methods.rb
+++ tests/cases/string_reflection_methods.rb
@@ -3,7 +3,10 @@ methods = [
   :casecmp?,
   :partition,
   :rpartition,
-  :byteslice
+  :byteslice,
+  :concat,
+  :prepend,
+  :slice!
 ]
 
 p methods.map { |m| "abc".respond_to?(m) }
@@ -13,3 +16,8 @@ p "abc".casecmp?("ABC")
 p "abc:def".partition(":")
 p "abc:def:ghi".rpartition(":")
 p "abc".byteslice(1, 2)
+s = "a"
+p s.concat("b")
+p s.prepend("z")
+p s.slice!(1, 1)
+p s
